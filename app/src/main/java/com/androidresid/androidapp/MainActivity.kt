461c5c32db0e8b3b846120dc964f7c83510ed280
Rustem <rustem.saitkulov@agoda.com>
2016-08-31 12:03:02 +0700

    Init commit

---
+++ app/src/main/java/com/androidresid/androidapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.androidresid.androidapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/test/kotlin/com/androidresid/androidapp/KotlinJunitTest.kt
@@ -0,0 +1,14 @@
+package com.androidresid.androidapp
+
+import org.junit.Assert
+import org.junit.Test
+
+class KotlinJunitTest {
+
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+
+}
+

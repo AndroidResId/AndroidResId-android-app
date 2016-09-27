eda496a0f87d6fa13e110f5cafcea8dbdac57084
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-27 13:18:49 +0300

    added robolectric test to find Toolbar and drawer on MainActivity

---
+++ app/src/test/kotlin/com/androidresid/androidapp/TestView.kt
@@ -0,0 +1,30 @@
+package com.androidresid.androidapp
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import kotlin.test.assertNotNull
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
+
+
+class MainActivityTest {
+
+    @Test
+    fun itShouldDisplayToolbar(){
+        val activity = Robolectric.setupActivity(MainActivity::class.java)
+        assertNotNull(activity.findViewById(R.id.toolbar))
+    }
+    @Test
+    fun itShouldDisplayNavigationDrawer(){
+        val activity = Robolectric.setupActivity(MainActivity::class.java)
+        assertNotNull(activity.findViewById(R.id.drawer))
+    }
+
+}
+
+

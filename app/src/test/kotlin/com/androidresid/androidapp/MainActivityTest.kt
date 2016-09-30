4242736d8b1caf42013073cc2fbbd5be2fef0f85
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-30 10:48:16 +0300

    refactoring use setup fun

--- app/src/test/kotlin/com/androidresid/androidapp/MainActivityTest.kt
+++ app/src/test/kotlin/com/androidresid/androidapp/MainActivityTest.kt
@@ -1,7 +1,9 @@
 package com.androidresid.androidapp
 
+import android.app.Activity
 import android.os.Build
 import android.view.View
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -16,37 +18,42 @@ import kotlin.test.assertTrue
 @Config(constants = BuildConfig::class, manifest = "AndroidManifest.xml", sdk = intArrayOf(Build.VERSION_CODES.LOLLIPOP))
 class MainActivityTest {
 
-    val activity = Robolectric.buildActivity(MainActivity::class.java)
-            .create()
-            .start()
-            .resume()
-            .visible()
-            .get()
+    private var activity: Activity? = null
+
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(MainActivity::class.java)
+                .create()
+                .start()
+                .resume()
+                .visible()
+                .get()
+    }
 
     @Test
     fun isShouldDisplayTitle() {
-        assertTrue(activity.title.toString() == ("AndroidResId"))
+        assertTrue(activity!!.title.toString() == ("AndroidResId"))
     }
 
     @Test
     fun itShouldDisplayToolbar() {
-        val toolbar = activity.findViewById(R.id.toolbar)
+        val toolbar = activity!!.findViewById(R.id.toolbar)
         assertNotNull(toolbar)
         assertTrue(toolbar.visibility == View.VISIBLE)
     }
 
     @Test
     fun itShouldDisplayNavigationDrawer() {
-        val drawer = activity.findViewById(R.id.drawer)
+        val drawer = activity!!.findViewById(R.id.drawer)
         assertNotNull(drawer)
         assertTrue(drawer.visibility == View.VISIBLE)
     }
 
     @Test
-    fun itShouldMenuItemSetting() {
+    fun itShouldHaveMenuItemSetting() {
         val menu = RoboMenuItem(R.id.action_settings)
         assertTrue(menu.isVisible)
-        activity.onOptionsItemSelected(menu)
+        activity!!.onOptionsItemSelected(menu)
         assertTrue(ShadowToast.getTextOfLatestToast() == ("Settings"))
     }
 

28e3a66f307a18399bab21f0f8b67b1d44a3a4df
Rustem Saitkulov <srmsam@yandex.ru>
2016-12-21 09:28:49 +0700

    Splash screen added

--- app/src/main/java/com/androidresid/androidapp/launch/LaunchActivity.kt
+++ app/src/main/java/com/androidresid/androidapp/launch/LaunchActivity.kt
@@ -2,11 +2,16 @@ package com.androidresid.androidapp.launch
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.androidresid.androidapp.R
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.verticalLayout
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        verticalLayout {
+            backgroundResource = R.mipmap.ic_launcher
+        }
     }
 }

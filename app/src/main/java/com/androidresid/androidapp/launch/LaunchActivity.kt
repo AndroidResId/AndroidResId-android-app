96f296700ea2debb07caed64c6fd2829dea8f45b
Rustem <Rustem.Saitkulov@agoda.com>
2017-02-25 11:55:30 +0700

    Dependencies upgrade Splash screen

--- app/src/main/java/com/androidresid/androidapp/launch/LaunchActivity.kt
+++ app/src/main/java/com/androidresid/androidapp/launch/LaunchActivity.kt
@@ -1,17 +1,49 @@
 package com.androidresid.androidapp.launch
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.widget.LinearLayout
+import com.androidresid.androidapp.MainActivity
 import com.androidresid.androidapp.R
-import org.jetbrains.anko.backgroundResource
-import org.jetbrains.anko.verticalLayout
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import org.jetbrains.anko.*
+import java.util.concurrent.TimeUnit
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        verticalLayout {
-            backgroundResource = R.mipmap.ic_launcher
+        frameLayout {
+            linearLayout {
+                gravity = Gravity.CENTER
+                orientation = LinearLayout.VERTICAL
+
+                imageView {
+                    imageResource = R.drawable.logo
+                }
+
+                textView {
+                    text = getString(R.string.app_name)
+                    gravity = Gravity.CENTER
+                    textSize = 20f
+                }.lparams {
+                    topMargin = dip(16)
+                }
+            }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Observable.timer(3, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    val i = Intent(this, MainActivity::class.java)
+                    startActivity(i)
+                    finish()
+                })
     }
 }
